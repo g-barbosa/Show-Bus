4fdc43a3bee7460ec3434833c59db74fb1106e21
g-barbosa <giovanne.bbarbosa@gmail.com>
2019-09-19 10:27:05 -0300

    codigo refatorado

--- src/components/BuscaBus.js
+++ src/components/BuscaBus.js
@@ -1,28 +1,21 @@
 
 import React, {useState, useEffect} from 'react'
-import '../App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import axios from 'axios'
+import Tabela from './Tabela'
 
-const BuscaBus = () => {
-  const [linha, setLinha] = useState('')
-  const [numero, setNumero] = useState('')
+function BuscaBus(props) {
   const [veiculos, setVeiculos] = useState([])
-
-  const onChange = event => {
-    setNumero(event.target.value)
-  }
 
   useEffect(() => {
     axios
-    .get('https://noxxonsat-nxnet.appspot.com/rest/usuarios/v2?linha='+linha, {
+    .get('https://noxxonsat-nxnet.appspot.com/rest/usuarios/v2?linha='+props.linha, {
       headers: {
         'Content-Type': 'application/json' ,
-        'Authorization': 'ENTRE NO SITE DA EMTU PARA PEDIR UMA CHAVE'
+        'Authorization': 'Basic YmxvZ3Nkb2dpbzpibG9nc2RvZ2lvQDEyMzQ1Ng=='
       }
     })
     .then(response => {
-
       let linhas = []
       response.data.linhas[0].veiculos.forEach(i => {
         linhas.push({
@@ -33,18 +26,15 @@ const BuscaBus = () => {
           longitude: i.longitude
         })
       })
+      console.log(veiculos)
       setVeiculos(linhas)
     })
-  },[linha])
+    .catch(err => {
+
+    })
+  },[props.linha])
 
   return (
-    <div className='App'>
-    <div className="container">
-    <br/>
-      <input placeholder='digite o numero da linha' onChange={onChange}/>
-      <button className='btn btn-success' onClick={() => {setLinha(numero)}}>OK</button>
-    </div>
-    <br/>
     <div className='container'>
       <table className='table'>
         <thead>
@@ -58,22 +48,17 @@ const BuscaBus = () => {
             {
               veiculos.map(i => {
                 return (
-                  <tr>
-                    <td>{i.linha}</td>
-                    <td>{i.sentido}</td>
-                    <td>{i.ponto}º</td>
-                    <td>
-                      <button className='btn btn-primary' 
-                        onClick={() => {window.open('https://www.google.com.br/maps/place/'+i.latitude+','+i.longitude)}}>
-                      VER NO MAPA</button>
-                    </td>
-                  </tr>
+                  <Tabela 
+                  linha={i.linha} 
+                  sentido={i.sentido} 
+                  arada={i.ponto} 
+                  latitude={i.latitude} 
+                  longitude={i.longitude}/>
                 )
               })
             }
         </tbody>
       </table>
-    </div>
     </div>
   );
 }
